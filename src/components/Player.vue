912a61110ba4f518be1af2c4a8e1ddb77d01beb7
jamiecode8 <jamiechiu2020@gmail.com>
2022-01-30 19:49:02 +0800

    project init

---
+++ src/components/Player.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="player">
+        <div class="player-header">
+            <h1>Player List Demo</h1>
+            <div class="player-header-user">這裡放頭像</div>
+        </div>
+        <div class="player-search">
+            <div class="player-search-team">
+                <label for="team">Team:</label>
+                <select name="team" id="team" >
+                    <option>All</option>
+                    <option></option>
+                </select>
+            </div>
+            <div  class="player-search-keyword">
+                <label for="keyword">Keywords:</label>
+                <input type="text">
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+</script>
